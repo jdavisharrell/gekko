b37c853855922c3800b010bf7f96fb713e755efd
jdavisharrell <37199769+jdavisharrell@users.noreply.github.com>
2018-03-20 12:58:49 -0400

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,15 +4,14 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
     host: '127.0.0.1',
     port: 3000,
-    timeout: 120000 // 2 minutes
   },
   ui: {
     ssl: false,
-    host: 'localhost',
+    host: '167.99.14.228',
     port: 3000,
     path: '/'
   },
